e5d4bdad56c6d9429a84e89dcecc226ca9f2f49a
golamafifahnaf <golamafifahnaf@gmail.com>
2025-07-15 22:56:38 +0600

    Updated CSES Problem Set

---
+++ CSES Problem Set/Sorting and Searching/Distinct Numbers.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, x;
+    cin >> n;
+    set<int> s;
+    while(n--){
+        cin >> x;
+        s.insert(x);
+    }
+    cout << s.size();
+    return 0;
+}
